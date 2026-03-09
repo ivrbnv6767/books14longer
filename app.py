c95d986cf6be04c953e86ecdd70dddb84e431ead
ivrbnv6767 <ivrbnv1@gmail.com>
2026-03-09 12:11:43 +0200

    Fix formatting and indentation in author search

--- app.py
+++ app.py
@@ -55,9 +55,11 @@ st.write("The cheapest book")
 st.write(cheapest)
 
 st.header(" Търсене по автор")
-search_author = st.text_input("Вúведи име Hа автор") if st.button("Тьрси по автор"):
-found = False
-for book in st.session_state.books: if book["author"] == search_author: st.write(book)
-found = True
-if found == False:
-st.write("Няма намерени книги от този автор.")
+search_author = st.text_input("Вúведи име Hа автор") 
+if st.button("Тьрси по автор"):
+    found = False
+for book in st.session_state.books: 
+  if book["author"] == search_author: st.write(book)
+    found = True
+    if found == False:
+      st.write("Няма намерени книги от този автор.")
